675bc071c88bdf8a0df752614bf551b7f9e9fac9
XuanDu-303 <phamxuandu13102003@gmail.com>
2026-01-06 21:32:07 +0700

    feat: set up basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/auth";
+import Chat from "./pages/chat";
+import Profile from "./pages/profile";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;

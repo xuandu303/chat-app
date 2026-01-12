ed1219159ee3b72090e2f76e5fece373484d38d7
XuanDu-303 <phamxuandu13102003@gmail.com>
2026-01-13 00:36:26 +0700

    fix: move chat containers into components

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -2,9 +2,9 @@ import { toast } from "sonner";
 import { useAppStore } from "@/store";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import ContactsContainer from "./contacts-container";
-import EmptyChatContainer from "./empty-chat-container";
-import ChatContainer from "./components";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
   const { userInfo } = useAppStore();

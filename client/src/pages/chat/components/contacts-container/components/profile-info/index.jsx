03b34232acc11d8867ec96a87f22b85455d25154
XuanDu-303 <phamxuandu13102003@gmail.com>
2026-01-14 08:17:55 +0700

    add profile info component

---
+++ client/src/pages/chat/components/contacts-container/components/profile-info/index.jsx
@@ -0,0 +1,83 @@
+import { useAppStore } from "@/store";
+import { getColor } from "@/lib/utils";
+import React from "react";
+import { HOST } from "@/utils/constants";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { FiEdit2 } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { TbLogout } from "react-icons/tb";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+const ProfileInfo = () => {
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+  const logOut = async () => {};
+  return (
+    <div className="absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]">
+      <div className="flex gap-3 items-center justify-center">
+        <div className="h-12 w-12 relative">
+          <Avatar className="w-12 h-12 rounded-full overflow-hidden">
+            {userInfo.image ? (
+              <AvatarImage
+                src={`${HOST}/${userInfo.image}`}
+                alt="profile"
+                className="object-cover rounded-full w-full h-full bg-black"
+              />
+            ) : (
+              <div
+                className={`uppercase h-12 w-12 text-lg border-[1px flex items-center justify-center rounded-full ${getColor(
+                  userInfo.color
+                )}`}
+              >
+                {userInfo.firstName
+                  ? userInfo.firstName.split("").shift()
+                  : userInfo.email.split("").shift()}
+              </div>
+            )}
+          </Avatar>
+        </div>
+        <div>
+          {userInfo.firstName && userInfo.lastName
+            ? `${userInfo.firstName} ${userInfo.lastName}`
+            : ""}
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <FiEdit2
+                className="text-purple-500 text-xl font-medium cursor-pointer"
+                onClick={() => navigate("/profile")}
+              />
+              <TooltipContent className="bg-[#1b1b1e] rounded-sm text-white p-1">
+                Edit Profile
+              </TooltipContent>
+            </TooltipTrigger>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <TbLogout
+                className="text-purple-500 text-2xl font-medium cursor-pointer"
+                onClick={logOut}
+              />
+              <TooltipContent className="bg-[#1b1b1e] rounded-sm text-white p-1 ">
+                Logout
+              </TooltipContent>
+            </TooltipTrigger>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
